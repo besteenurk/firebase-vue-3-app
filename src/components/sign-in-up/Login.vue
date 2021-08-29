ec22b118cf4de46568912ae4b72555c6ece9902d
besteenurk <besteenurk@gmail.com>
2021-08-29 20:47:22 +0300

    added login, sign up file

--- src/components/sign-in-up/Login.vue
+++ src/components/sign-in-up/Login.vue
@@ -1,14 +1,39 @@
 <template>
-  <div class="content">
+  <form @submit.prevent="handleSubmit" class="content">
     <h1>Left Div</h1>
-    <input type="email" placeholder="Email" />
-    <input type="password" placeholder="Password" />
+    <input type="email" placeholder="Email" v-model="email" />
+    <input type="password" placeholder="Password" v-model="password" />
+    <div class="error">{{ error }}</div>
     <button class="btn-signin">Sign Up</button>
-  </div>
+  </form>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+import Login from "../../composables/Login";
+
+export default {
+  setup(props, context) {
+    const email = ref("");
+    const password = ref("");
+
+    const { error, login } = Login();
+
+    const handleSubmit = async () => {
+      await login(email.value, password.value);
+
+      if (!error.value) {
+        context.emit("login");
+      }
+    };
+    return {
+      email,
+      password,
+      handleSubmit,
+      error,
+    };
+  },
+};
 </script>
 
 <style></style>
--- src/components/sign-in-up/SignUp.vue
+++ src/components/sign-in-up/SignUp.vue
@@ -1,15 +1,40 @@
 <template>
-  <div class="content">
+  <form class="content" @submit.prevent="handleSubmit">
     <h1>Right Div</h1>
-    <input type="text" placeholder="Username" />
-    <input type="email" placeholder="Email" />
-    <input type="password" placeholder="Password" />
+    <input type="text" placeholder="Username" v-model="displayName" />
+    <input type="email" placeholder="Email" v-model="email" />
+    <input type="password" placeholder="Password" v-model="password" />
+    <div class="error">{{ error }}</div>
     <button class="btn-signup">Sign Up</button>
-  </div>
+  </form>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+import SignUp from "../../composables/SignUp";
+
+export default {
+  setup(props, context) {
+    const { error, signup } = SignUp();
+    const displayName = ref("");
+    const email = ref("");
+    const password = ref("");
+
+    const handleSubmit = async () => {
+      await signup(email.value, password.value, displayName.value);
+      if (!error.value) {
+        context.emit("signup");
+      }
+    };
+    return {
+      displayName,
+      email,
+      password,
+      handleSubmit,
+      error,
+    };
+  },
+};
 </script>
 
 <style></style>
