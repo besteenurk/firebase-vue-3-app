5372e9c602517d93995e29fbeb18b4b990c8f334
besteenurk <besteenurk@gmail.com>
2021-08-29 19:45:18 +0300

    updated firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,3 +1,8 @@
+import firebase from "firebase/app";
+import "firebase/firestore"
+import 'firebase/auth'
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyC8kyNQ_nnpfEmSk5Ua9MUQzkItQPjP1FE",
     authDomain: "fir-vue-3-app.firebaseapp.com",
@@ -7,3 +12,12 @@ const firebaseConfig = {
     appId: "1:1062406189911:web:7f9841efd9e0a656ebc412",
     measurementId: "G-1GXFME5VEK"
 };
+
+firebase.initializeApp(firebaseConfig)
+const firebaseAuth = firebase.auth()
+const firebaseFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export default {
+    firebaseAuth, firebaseFirestore, timestamp
+}
