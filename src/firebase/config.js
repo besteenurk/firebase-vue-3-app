72b1673a16e2f83965718fdc372ae5ac3e344f8b
besteenurk <besteenurk@gmail.com>
2021-08-29 17:00:15 +0300

    firebase added

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC8kyNQ_nnpfEmSk5Ua9MUQzkItQPjP1FE",
+    authDomain: "fir-vue-3-app.firebaseapp.com",
+    projectId: "fir-vue-3-app",
+    storageBucket: "fir-vue-3-app.appspot.com",
+    messagingSenderId: "1062406189911",
+    appId: "1:1062406189911:web:7f9841efd9e0a656ebc412",
+    measurementId: "G-1GXFME5VEK"
+};
